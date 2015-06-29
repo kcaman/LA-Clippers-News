eb406f897d10bd35f9d52a302610e649ce8a4701
psrivongse <psrivongse@gmail.com>
2015-06-28 19:27:42 -0700

    artifacts in compilation

--- Gemfile
+++ Gemfile
@@ -2,22 +2,16 @@
 # the following line to use "https"
 source 'http://rubygems.org'
 
-<<<<<<< HEAD
 gem "middleman", "~>3.1.4"
-=======
-gem "middleman", "~>3.1.3"
->>>>>>> master
 
 # Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
 
-<<<<<<< HEAD
 # For faster file watcher updates on Windows:
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
-=======
+
 # For faster file watcher updates:
 # gem "wdm", "~> 0.1.0") # Windows
->>>>>>> master
 
 # Cross-templating language block fix for Ruby 1.8
 platforms :mri_18 do
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,57 +4,57 @@ function start(){
 	var url = "http://sports.yahoo.com/nba/teams/lac/rss.xml";
 	var links = new Array();
 	if(url.match('^http')){
-
 		$.getJSON("http://query.yahooapis.com/v1/public/yql?"+
-            "q=select%20*%20from%20html%20where%20url%3D%22"+
-            encodeURIComponent(url)+
-            "%22&format=xml'&callback=?", 
-			function(data){
-				if(data.results[0]){						
-					var data = filterData(data.results[0]);
-					urls = parseLinks(data);
-					var divs = $('.content div');
-					var j = 0;
-					for(count = 0; count < 3; count++){
-						doAjax(urls[count], divs.eq(j));
-						j++;
-					}				
-				} else{
-					var errormessage = '<p>Error: could not load the page.</p>';
-					target.html(errormessage);
-				}
-				
-				}
-		);
-	}else {
-		target.load(url);
-	}
+		"q=select%20*%20from%20html%20where%20url%3D%22"+
+		encodeURIComponent(url)+
+		"%22&format=xml'&callback=?", 
+		function(data){
+			if(data.results[0]){						
+				var data = filterData(data.results[0]);
+				urls = parseLinks(data);
+				var divs = $('.content div');
+				var j = 0;
+				for(count = 0; count < 3; count++){
+					doAjax(urls[count], divs.eq(j));
+					j++;
+				}				
+			} else {
+				var errormessage = '<p>Error: could not load the page.</p>';
+				target.html(errormessage);
+			}	
+		}
+	);
+} else {
+	target.load(url);
+}
 }
 
-/* Perform .getJSON on the specific links from the RSS Feed, and filters the data returned.
+/* 
+Perform .getJSON on the specific links from the RSS Feed, and filters the data returned.
 */
 function doAjax(url, container){
 	$.getJSON("http://query.yahooapis.com/v1/public/yql?"+
-            "q=select%20*%20from%20html%20where%20url%3D%22"+
-            encodeURIComponent(url)+
-            "%22&format=xml'&callback=?", 
-			function(data){
-				if(data.results[0]){
-					var data = filterSite(data.results[0]);
-					container.html(data);
-				} else{
-					container.remove();
-					console.log(url);
-					createDivs();
-					var divs = $('.content div');
-					doAjax(urls[count], divs.eq(divs.length - 1));
-					count++;
-				}
+        "q=select%20*%20from%20html%20where%20url%3D%22"+
+        encodeURIComponent(url)+
+        "%22&format=xml'&callback=?", 
+		function(data){
+			if(data.results[0]){
+				var data = filterSite(data.results[0]);
+				container.html(data);
+			} else {
+				container.remove();
+				createDivs();
+				var divs = $('.content div');
+				doAjax(urls[count], divs.eq(divs.length - 1));
+				count++;
 			}
+		}
 	);
 }
 
-/* Create 2 more divs */
+/* 
+Create 2 more divs 
+*/
 function createDivs(){
 	$('.content').append('<div class="target"></div>');
 }
@@ -66,7 +66,6 @@ function filterSite(data){
 	data = filterData(data);
 	var headline = data.match(/<h1\ class=\"headline">(.*?)\/h1>/g);
 	data = data.replace(/<h1\ class=\"headline">(.*?)>/g, '');
-	//data = data.replace(/^.*?(?=<p\ class="first">)/g, '');
 	data = data.replace(/^.*?(?=<!--\ google_ad_section_start\ -->)/g, '');
 	data = data.match(/^.*(?=<!--\ google_ad_section_end\ -->)/g);
 	data = data[0];
@@ -100,6 +99,3 @@ $(document).ready(function(){
 	var target = $('.target');
 	start();
 });
-=======
-//= require_tree .
->>>>>>> master
